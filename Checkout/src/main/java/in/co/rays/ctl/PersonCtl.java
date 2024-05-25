bd84de7ec9535c82c343abfc73b9a9cce5036169
unknown <Krishnaa>
2024-05-25 14:33:48 +0530

    done

---
+++ Checkout/src/main/java/in/co/rays/ctl/PersonCtl.java
@@ -0,0 +1,12 @@
+package in.co.rays.ctl;
+
+public class PersonCtl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("ABC");
+
+	}
+
+}
---
+++ Checkout/src/main/java/in/co/rays/dto/PersonDTO.java
@@ -0,0 +1,17 @@
+package in.co.rays.dto;
+
+public class PersonDTO {
+	
+	private int name;
+
+	public int getName() {
+		return name;
+	}
+
+	public void setName(int name) {
+		this.name = name;
+	}
+	
+	
+
+}
